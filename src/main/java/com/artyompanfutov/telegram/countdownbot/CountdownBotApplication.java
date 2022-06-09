20c41ae629ec2fad7758350ef7260b90a3cc5252
Artyom Panfutov <artyom.cox@gmail.com>
2022-06-09 13:04:35 +0400

    init commit

---
+++ src/main/java/com/artyompanfutov/telegram/countdownbot/CountdownBotApplication.java
@@ -0,0 +1,13 @@
+package com.artyompanfutov.telegram.countdownbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountdownBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountdownBotApplication.class, args);
+	}
+
+}
